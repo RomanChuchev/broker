5358b961989f33302e8a7db3d8db18c8b85f3b46
romanchuchev <romanok20@gmail.com>
2023-01-07 17:21:45 +0300

    feat: add adaptive section investing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,27 @@
 import s from "./Home.module.scss";
-import img1 from "../../assets/images/bitcoin.png";
-import img2 from "../../assets/images/bg-1.png";
 import cl from "classnames";
 import Button from "../../components/Button";
 const Home = () => {
   return (
-    <section className={cl(s.investing, "container")}>
-      <img src={img1} alt="bg" className={s.img1} />
-      <div className={s.description}>
-        <h2 className={s.title}>
-          Инвестируй с&nbsp;лучшими&nbsp;крипто-трейдерами
-        </h2>
-        <p className={s.text}>
-          Сервис автоматического копирования сделок на&nbsp;криптовалютных
-          биржах.
-        </p>
-        <Button className="investing" onClick={() => {}}>
-          Создать аккаунт
-        </Button>
-      </div>
+    <section className={cl(s.investing)}>
+      <div className={cl(s.container, "container")}>
+        <div className={s.description}>
+          <h2 className={s.title}>
+            Инвестируй с&nbsp;лучшими&nbsp;крипто-трейдерами
+          </h2>
+          <p className={s.text}>
+            Сервис автоматического копирования сделок на&nbsp;криптовалютных
+            биржах.
+          </p>
+          <Button className="investing" onClick={() => {}}>
+            Создать аккаунт
+          </Button>
+        </div>
 
-      <div className={s.man}>
-        <img src={img2} alt="bg" className={s.img2} />
-        <p className={s.text1}>Пассивный зароботок</p>
-        <p className={s.text2}>Публичный трейдинг</p>
+        <div className={s.man}>
+          <p className={s.text1}>Пассивный зароботок</p>
+          <p className={s.text2}>Публичный трейдинг</p>
+        </div>
       </div>
     </section>
   );
